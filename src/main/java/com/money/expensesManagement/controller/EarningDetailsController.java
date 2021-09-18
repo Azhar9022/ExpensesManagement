7d84aeac102b2fb0252cf6970d57865677381d5a
Mohd Azhar <azhar9889@gmail.com>
2021-09-18 20:29:00 +0530

    Expense Details related code

--- src/main/java/com/money/expensesManagement/controller/EarningDetailsController.java
+++ src/main/java/com/money/expensesManagement/controller/EarningDetailsController.java
@@ -1,14 +1,43 @@
 package com.money.expensesManagement.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.money.expensesManagement.modelImpl.Earning;
+import com.money.expensesManagement.serviceImpl.EarningService;
 
 @Controller
 public class EarningDetailsController {
+	
+	@Autowired
+	EarningService earningService;
+	
 	@RequestMapping("/earningDetails")
 	public String getEarningDetails() {
 		
 		return "earningDetails";
 	}
+	
+	@GetMapping(path="/getEarningDetails")
+	public ModelAndView getAllEarning(){
+		List<Earning> listEarning = earningService.getAllEarning();
+		 //return new ResponseEntity<List<Earning>>(listEarning, HttpStatus.OK);
+		 return new ModelAndView("earningDetails", "earningDetailForm", listEarning);
+	}
+	
+	/*
+	 * @GetMapping(path="/getEarning/{id}") public ModelAndView
+	 * getEarningById(@PathVariable("id") int id) { Earning earningObj =
+	 * earningService.getEarningById(id); return new ModelAndView("earning",
+	 * "earningForm", earningObj); }
+	 */
 
 }
--- src/main/webapp/WEB-INF/jsp/earningDetails.jsp
+++ src/main/webapp/WEB-INF/jsp/earningDetails.jsp
@@ -1,16 +1,28 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 
 <html>
-<table>
-
-<c:forEach var = "i" items="1,4,5,6,7,8,9">
+<form method="post" modelAttribute="earningDetailForm" action="/saveEarning">
+<table border = "1">
 <tr>
-    <td>Item <c:out value = "No. ${i}"/></td>
-    <td>2nd td </td>
-    <td><input type="button" value="delete"/></td>
+<td>Earning Date 	</td>
+<td>Earning Source  </td>
+<td>Earning Payment </td> 
+<td>Earning Remarks </td> 
+<td></td>
+</tr>
+<c:forEach var = "earnObj" items="${earningDetailForm}">
+<tr>
+    
+    <td><c:out value = "${earnObj.earnDate}"/></td>
+    <td><c:out value = "${earnObj.earnSource}"/></td>
+    <td><c:out value = "${earnObj.earnPayment}"/></td> 
+     <td><c:out value = "${earnObj.earnRemarks}"/></td> 
+  <td><input type="button" value="delete"/></td>
  </tr>
 </c:forEach>
 
 
 </table>
+</form>
 </html>
